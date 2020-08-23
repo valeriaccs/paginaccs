4e28edeedabdde816087f8f7e491883e9c3bf6b3
valeriaccs <68400943+valeriaccs@users.noreply.github.com>
2020-08-23 22:35:52 +0200

    Add files via upload

---
+++ mmmmm.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>VIDEO</title>
+</head>
+
+<body>
+	<div>
+     
+      <p><a href="https://youtu.be/oygrmJFKYZY"><img src="CCS.png" width="235" height="231" alt=""/></a></p>
+	</div>
+</body>
+</html>
